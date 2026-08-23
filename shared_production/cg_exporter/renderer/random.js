29c8e0bb37ddef08b60d20d469b1a0dc3a596519
Ryan Qu <hqu35@wisc.edu>
2026-08-24 07:00:05 +0800

    Update random.js (Phase 2.11 audit)

--- shared_production/cg_exporter/renderer/random.js
+++ shared_production/cg_exporter/renderer/random.js
@@ -16,6 +16,7 @@ function choice(arr) {
   return arr[randInt(0, arr.length - 1)];
 }
 
+// Fisher-Yates shuffle -> P = 1/n!
 function shuffle(arr) {
   const copy = [...arr];
   for (let i = copy.length - 1; i > 0; i--) {
@@ -24,21 +25,24 @@ function shuffle(arr) {
   }
   return copy;
 }
-
+// FNV-1a hash 
 function hashString(str) {
   let h = 2166136261;
   for (let i = 0; i < str.length; i++) {
     h ^= str.charCodeAt(i);
     h = Math.imul(h, 16777619);
   }
+  // cnovert to unsigned 32 bit
   return h >>> 0;
 }
-
+// PRNG
 function mulberry32(a) {
   return function next() {
     let t = (a += 0x6d2b79f5);
+    // avalanch mixing step
     t = Math.imul(t ^ (t >>> 15), t | 1);
     t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+    // output [0,1)
     return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
   };
 }
